1679258cf6e0087249e317fc0e2fba3360389412
psophometric <jonathan.lautrou@gmail.com>
2017-02-10 21:51:30 +0100

    Create ttn-abp-gps.cpp

---
+++ examples/raspi/ttn-abp-gps/ttn-abp-gps.cpp
@@ -0,0 +1,363 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Fork Psophometric : to send gps coordinates payload. 
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example sends a valid LoRaWAN packet with payload {lat, lon},
+ * using frequency and encryption settings matching those of
+ * the The Things Network.
+ *
+ * This uses ABP (Activation-by-personalisation), where a DevAddr and
+ * Session keys are preconfigured (unlike OTAA, where a DevEUI and
+ * application key is configured, while the DevAddr and session keys are
+ * assigned/generated in the over-the-air-activation procedure).
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+ * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+ * violated by this sketch when left running for longer)!
+ *
+ * To use this sketch, first register your application and device with
+ * the things network, to set or generate a DevAddr, NwkSKey and
+ * AppSKey. Each device should have their own unique values for these
+ * fields.
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ *******************************************************************************/
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <lmic.h>
+#include <hal/hal.h>
+
+#include <vector>          // split() function
+#include <functional>      // split() function
+#include <iostream>        // split() function
+using namespace std;       // split() function
+
+// TTN-ABP
+// LoRaWAN NwkSKey, network session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const PROGMEM u1_t NWKSKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
+
+// LoRaWAN AppSKey, application session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const u1_t PROGMEM APPSKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
+
+// LoRaWAN end-device address (DevAddr)
+static const u4_t DEVADDR = 0x03FF0001 ; // <-- Change this address for every node!
+
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty)
+// cycle limitations).
+const unsigned TX_INTERVAL = 0;
+
+//Flag for Ctrl-C
+volatile sig_atomic_t force_exit = 0;
+
+// LoRasPi board
+// see https://github.com/hallard/LoRasPI
+//#define RF_LED_PIN RPI_V2_GPIO_P1_16 // Led on GPIO23 so P1 connector pin #16
+//#define RF_CS_PIN  RPI_V2_GPIO_P1_24 // Slave Select on CE0 so P1 connector pin #24
+//#define RF_IRQ_PIN RPI_V2_GPIO_P1_22 // IRQ on GPIO25 so P1 connector pin #22
+//#define RF_RST_PIN RPI_V2_GPIO_P1_15 // RST on GPIO22 so P1 connector pin #15
+
+// Raspberri PI Lora Gateway for multiple modules
+// see https://github.com/hallard/RPI-Lora-Gateway
+// Module 1 on board RFM95 868 MHz (example)
+//#define RF_LED_PIN RPI_V2_GPIO_P1_07 // Led on GPIO4 so P1 connector pin #7
+#define RF_CS_PIN  RPI_V2_GPIO_P1_24 // Slave Select on CE0 so P1 connector pin #24
+#define RF_IRQ_PIN RPI_V2_GPIO_P1_22 // IRQ on GPIO25 so P1 connector pin #22
+#define RF_RST_PIN RPI_V2_GPIO_P1_11 // Reset on GPIO17 so P1 connector pin #11
+
+
+// Dragino Raspberry PI hat (no onboard led)
+// see https://github.com/dragino/Lora
+//#define RF_CS_PIN  RPI_V2_GPIO_P1_22 // Slave Select on GPIO25 so P1 connector pin #22
+//#define RF_IRQ_PIN RPI_V2_GPIO_P1_07 // IRQ on GPIO4 so P1 connector pin #7
+//#define RF_RST_PIN RPI_V2_GPIO_P1_11 // Reset on GPIO17 so P1 connector pin #11
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss  = RF_CS_PIN,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst  = LMIC_UNUSED_PIN, //RF_RST_PIN,
+    .dio  = {LMIC_UNUSED_PIN, LMIC_UNUSED_PIN, LMIC_UNUSED_PIN},
+};
+
+#ifndef RF_LED_PIN
+#define RF_LED_PIN NOT_A_PIN
+#endif
+
+
+// https://www.safaribooksonline.com/library/view/c-cookbook/0596007612/ch04s07.html
+void split(const string& s, char c,
+           vector<string>& v) {
+   string::size_type i = 0;
+   string::size_type j = s.find(c);
+
+   while (j != string::npos) {
+      v.push_back(s.substr(i, j-i));
+      i = ++j;
+      j = s.find(c, j);
+
+      if (j == string::npos)
+         v.push_back(s.substr(i, s.length()));
+   }
+}
+
+void do_send(osjob_t* j) {
+    char strTime[16];
+    string s;
+    vector<string> v;
+    getSystemTime(strTime , sizeof(strTime));
+    printf("%s - waiting to send.. \n", strTime);
+
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        printf("OP_TXRXPEND, not sending\n");
+    } else {
+        digitalWrite(RF_LED_PIN, HIGH);
+
+        cin >> s;
+        getSystemTime(strTime , sizeof(strTime));
+        cout << ("%s", strTime) << " - " << s << endl;
+        split(s, ',', v);
+        float float_latitude = ::atof(v[0].c_str());
+        float float_longitude = ::atof(v[1].c_str());
+
+        // https://www.thethingsnetwork.org/forum/t/best-practices-when-sending-gps-location-data/1242/13
+        uint8_t mydata[6];
+        int32_t lat = float_latitude * 10000;
+        int32_t lon = float_longitude * 10000;
+        // Pad 2 int32_t to 6 8uint_t, skipping the last byte (x >> 24)
+        mydata[0] = lat;
+        mydata[1] = lat >> 8;
+        mydata[2] = lat >> 16;
+        mydata[3] = lon;
+        mydata[4] = lon >> 8;
+        mydata[5] = lon >> 16;
+        LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+        getSystemTime(strTime , sizeof(strTime));
+        printf("%s - Packet queued\n", strTime);
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void onEvent (ev_t ev) {
+    char strTime[16];
+    getSystemTime(strTime , sizeof(strTime));
+    printf("%s - ", strTime);
+
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            printf("EV_SCAN_TIMEOUT\n");
+        break;
+        case EV_BEACON_FOUND:
+            printf("EV_BEACON_FOUND\n");
+        break;
+        case EV_BEACON_MISSED:
+            printf("EV_BEACON_MISSED\n");
+        break;
+        case EV_BEACON_TRACKED:
+            printf("EV_BEACON_TRACKED\n");
+        break;
+        case EV_JOINING:
+            printf("EV_JOINING\n");
+        break;
+        case EV_JOINED:
+            printf("EV_JOINED\n");
+            digitalWrite(RF_LED_PIN, LOW);
+            // Disable link check validation (automatically enabled
+            // during join, but not supported by TTN at this time).
+            LMIC_setLinkCheckMode(0);
+        break;
+        case EV_RFU1:
+            printf("EV_RFU1\n");
+        break;
+        case EV_JOIN_FAILED:
+            printf("EV_JOIN_FAILED\n");
+        break;
+        case EV_REJOIN_FAILED:
+            printf("EV_REJOIN_FAILED\n");
+        break;
+        case EV_TXCOMPLETE:
+            printf("EV_TXCOMPLETE (includes waiting for RX windows)\n");
+            if (LMIC.txrxFlags & TXRX_ACK)
+              printf("%s Received ack\n", strTime);
+            if (LMIC.dataLen) {
+              printf("%s Received %d bytes of payload\n", strTime, LMIC.dataLen);
+            }
+            digitalWrite(RF_LED_PIN, LOW);
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+        break;
+        case EV_LOST_TSYNC:
+            printf("EV_LOST_TSYNC\n");
+        break;
+        case EV_RESET:
+            printf("EV_RESET\n");
+        break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            printf("EV_RXCOMPLETE\n");
+        break;
+        case EV_LINK_DEAD:
+            printf("EV_LINK_DEAD\n");
+        break;
+        case EV_LINK_ALIVE:
+            printf("EV_LINK_ALIVE\n");
+        break;
+        default:
+            printf("Unknown event\n");
+        break;
+    }
+}
+
+/* ======================================================================
+Function: sig_handler
+Purpose : Intercept CTRL-C keyboard to close application
+Input   : signal received
+Output  : -
+Comments: -
+====================================================================== */
+void sig_handler(int sig)
+{
+  printf("\nBreak received, exiting!\n");
+  force_exit=true;
+}
+
+/* ======================================================================
+Function: main
+Purpose : not sure ;)
+Input   : command line parameters
+Output  : -
+Comments: -
+====================================================================== */
+int main(void)
+{
+    // caught CTRL-C to do clean-up
+    signal(SIGINT, sig_handler);
+
+    printf("%s Starting\n", __BASEFILE__);
+
+      // Init GPIO bcm
+    if (!bcm2835_init()) {
+        fprintf( stderr, "bcm2835_init() Failed\n\n" );
+        return 1;
+    }
+
+	// Show board config
+    printConfig(RF_LED_PIN);
+    printKeys();
+
+    // Light off on board LED
+    pinMode(RF_LED_PIN, OUTPUT);
+    digitalWrite(RF_LED_PIN, HIGH);
+
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+
+    // TTN-ABP
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    #ifdef PROGMEM
+    // On AVR, these values are stored in flash and only copied to RAM
+    // once. Copy them to a temporary buffer here, LMIC_setSession will
+    // copy them into a buffer of its own again.
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+
+    #if defined(CFG_eu868)
+    // Set up the channels used by the Things Network, which corresponds
+    // to the defaults of most gateways. Without this, only three base
+    // channels from the LoRaWAN specification are used, which certainly
+    // works, so it is good for debugging, but can overload those
+    // frequencies, so be sure to configure the full frequency range of
+    // your network here (unless your network autoconfigures them).
+    // Setting up channels should happen after LMIC_setSession, as that
+    // configures the minimal channel set.
+    // NA-US channels 0-71 are configured automatically
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // 1CH GATEWAY
+    LMIC_setupChannel(1, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7), BAND_CENTI);       // 1CH GATEWAY
+    LMIC_setupChannel(2, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // 1CH GATEWAY
+    LMIC_setupChannel(3, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // 1CH GATEWAY
+    LMIC_setupChannel(4, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // 1CH GATEWAY
+    LMIC_setupChannel(5, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // 1CH GATEWAY
+    LMIC_setupChannel(6, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // 1CH GATEWAY
+    LMIC_setupChannel(7, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // 1CH GATEWAY
+    LMIC_setupChannel(8, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_MILLI);      // 1CH GATEWAY
+    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+    // devices' ping slots. LMIC does not have an easy way to define set this
+    // frequency and support for class B is spotty and untested, so this
+    // frequency is not configured here.
+    #elif defined(CFG_us915)
+    // NA-US channels 0-71 are configured automatically
+    // but only one group of 8 should (a subband) should be active
+    // TTN recommends the second sub band, 1 in a zero based count.
+    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+    LMIC_selectSubBand(1);
+    #endif
+
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF9;
+
+    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,14);
+
+
+    // Start job (sending automatically starts OTAA too)
+    do_send(&sendjob);
+
+    while(!force_exit) {
+      os_runloop_once();
+
+      // We're on a multitasking OS let some time for others
+      // Without this one CPU is 99% and with this one just 3%
+      // On a Raspberry PI 3
+      usleep(1000);
+    }
+
+    // We're here because we need to exit, do it clean
+
+    // Light off on board LED
+    digitalWrite(RF_LED_PIN, LOW);
+
+    // module CS line High
+    digitalWrite(lmic_pins.nss, HIGH);
+    printf( "\n%s, done my job!\n", __BASEFILE__ );
+    bcm2835_close();
+    return 0;
+}
